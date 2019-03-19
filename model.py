7c88cb760fe0309c30b7be181f3b8404d398c27a
momongaclub <threeforest8@gmail.com>
2019-03-19 17:46:45 +0900

    fix syntax

--- model.py
+++ model.py
@@ -14,13 +14,9 @@ output: binary value.
 
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('config_file',
-                         help = 'config_file have any parametors.')
-    parser.add_argument('training_data',
-                         help = 'To training_data')
-    parser.add_argument('embeddings_data',
-                         help = 'To embeedings_data')
-
+    parser.add_argument('config_file', help='config_file have any parametors.')
+    parser.add_argument('training_data', help='To training_data')
+    parser.add_argument('embeddings_data', help='To embeedings_data')
     args = parser.parse_args()
     return args
 
@@ -35,8 +31,9 @@ class BILSTM_Model():
 
     def load_model(self):
         self.model = Sequential()
-        #model.add(LSTM(units = N_HIDDEN, input_shape=(LENGTH_OF_SEQUENCE, N_IN)))
-        self.model.add(Dense(units = int(self.configs['n_out']) ))
+        # model.add(LSTM(units = N_HIDDEN,
+        #                input_shape=(LENGTH_OF_SEQUENCE, N_IN)))
+        self.model.add(Dense(units=int(self.configs['n_out'])))
         self.model.add(Activation('sigmoid'))
 
     def load_config(self, config_file):
@@ -46,10 +43,10 @@ class BILSTM_Model():
                 name, value = line.split(':')
                 self.configs[name] = value
 
-    def compile(self):  
-        self.model.compile(loss = self.configs['loss'],
-                           optimizer = self.configs['optimizer'],
-                           metrics = [self.configs['metrics']])
+    def compile(self):
+        self.model.compile(loss=self.configs['loss'],
+                           optimizer=self.configs['optimizer'],
+                           metrics=[self.configs['metrics']])
 
 
 class Embeddings():
@@ -81,11 +78,11 @@ class Embeddings():
         for sentence in self.sentences:
             vector = []
             for word in sentence:
-                if self.embeddings.get(word, None) == None:
+                if self.embeddings.get(word, None) is None:
                     word = '<unk>'
                 else:
                     word = self.embeddings[word]
-                vector.append(word) 
+                vector.append(word)
             self.vectors.append(vector)
 
     def split_data(self):
@@ -97,17 +94,18 @@ class Embeddings():
 
 def main():
     args = parse()
-    bilstm_model = BILSTM_Model()
-    bilstm_model.load_config(args.config_file)
-    bilstm_model.load_model()
-    bilstm_model.compile()
-
     embeddings = Embeddings()
     embeddings.load_training_data(args.training_data)
     embeddings.load_embeddings(args.embeddings_data)
     embeddings.sentences2embeddings()
 
+    bilstm_model = BILSTM_Model()
+    bilstm_model.load_config(args.config_file)
+    bilstm_model.load_model()
+    bilstm_model.compile()
+
     bilstm_model.model.fit(embeddings.x_train, embeddings.y_train)
 
 if __name__ == '__main__':
     main()
+
