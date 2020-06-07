b7e856ddc1aa48f188e4989f2f3f1fb6a9018a31
momongaclub <threeforest8@gmail.com>
2020-06-07 17:44:57 +0900

    add UserClass

--- get_tweets.py
+++ get_tweets.py
@@ -26,25 +26,62 @@ def write_data(fname, all_user_data):
                 fp.writelines(datum + TAB)
             fp.write('\n')
 
-def text2sentnece(text):
+def text2sentence(text):
     text = text.split(SEP)
     # print('RT', text[0], 'bool', str(text[0]) == 'RT')
     sentence = ''.join(text)
     return sentence
 
 
+def is_location(location):
+    if location == '':
+        location = None
+    return location
+
+def is_url(entities):
+    url = ''
+    try:
+        url = entities['url']['urls'][0]['display_url']
+    except:
+        url = 'None'
+    return url
+
+def datetime2date(datetime):
+    date = datetime.split(SPACE)[0]
+    return date
+
 class User():
 
     def __init__(self):
         self.name = ''
         self.id = ''
+        self.description = ''
         self.location = ''
         self.url = ''
-        self.description = ''
         self.created_at = ''
         self.followers_count = 0
         self.friends_count = 0
         self.favorites_count = 0
+        self.texts = []
+
+    def write_data(self, fname):
+        with open(fname, 'a') as fp:
+            for line in self.__dict__.items():
+                if str(line[0]) == 'texts':
+                    sentences = ''
+                    for sentence in line[1]:
+                        sentences = sentences + sentence + TAB
+                    sentences.rstrip(TAB)
+                    fp.write(sentences)
+                else:
+                    fp.write(str(line[1]))
+                    fp.write(TAB)
+            fp.write(SEP)
+
+    def __str__(self):
+        for line in self.__dict__.items():
+            print(str(line))
+        return ''
 
 def main():
     args = parser() 
@@ -54,38 +91,34 @@ def main():
     api.get_user_names(args.count)
     all_user_data = []
     for user_name in api.user_names:
-        user_data = []
+        # user_data = []
         user = User()
         user.name = user_name
-        user_datum = api.api.user_timeline(user_name, count=1, page=1)
+        user_datum = api.api.user_timeline(user.name, count=1, page=1)
         user_datum = user_datum[RESULT]
-        print('user_id:', user_datum.user.screen_name)
-        if user_datum.user.location == '':
-            user_datum.user.location = 'None'
-        user.location = user_datum.user.location
-        print('location:', user_datum.user.location)
-        print('description:', user_datum.user.description)
-        if user_datum.user.entities.get('url') == None:
-            print('url:', 'None')
-        else:
-            print('url:', user_datum.user.entities['url']['urls'][0]['display_url'])
-        print('followers_count:', user_datum.user.followers_count)
-        print('friends_count:', user_datum.user.friends_count)
-        print('created_at:', user_datum.user.created_at)
-        print('favorites_count:', user_datum.user.favourites_count)
+        user.id = user_datum.id
+        user.screen_name = user_datum.user.screen_name
+        user.description = text2sentence(user_datum.user.description)
+        user.followers_count = user_datum.user.followers_count
+        user.friends_count = user_datum.user.friends_count
+        # TODO datetime format になってるので文字列に変更
+        user.created_at = datetime2date(str(user_datum.user.created_at))
+        user.favourites_count = user_datum.user.favourites_count
+        user.location = is_location(user_datum.user.location)
+        user.url = is_url(user_datum.user.entities)
         for page in range(1, args.page_num+1):
-            user_datum = api.api.user_timeline(user_name, count=args.count, page=page)
-            # user_datum = api.api.user_timeline('kagamiu055', count=args.count, page=page)
-            # user_datum = api.api.user_timeline('tyariRAD', count=args.count, page=page)
+            user_datum = api.api.user_timeline(user.name, count=args.count, page=page)
             try:
                 user_datum = user_datum[RESULT]
-                print('text:', text2sentnece(user_datum.text))
+                text = text2sentence(user_datum.text)
             except:
-                print('warning:', '0')
-            user_data.append(user_datum)
+                text ='None'
+            #user_data.append(user_datum)
+            user.texts.append(text)
         print(SEP)
-        print(user.name, user.location)
-        all_user_data.append(user_data)
+        print(user)
+        user.write_data(args.output_file)
+        all_user_data.append(user)
     # write_data(args.output_file, all_user_data)
 
 
